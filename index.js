92bb6b3b235a40b11e622f2d1684f72ea11d11ed
vignesh <vigneshit13@gmail.com>
2019-09-22 22:33:50 +0530

    Redis caching

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const redis = require('redis');
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+const app = express();
+
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} git repos</h2>`
+}
+
+async function getRepos(req, res, next) {
+  try {
+    console.log('Fetching data...');
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    const repos  = data.public_repos;
+    
+    client.setex(username, 3600, repos);
+    res.send(setResponse(username, repos));
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+//cache middleware
+
+function cache(req, res, next) {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if(err) throw err;
+
+    if(data != null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  })
+}
+
+app.get('/repos/:username',cache , getRepos);
+
+app.listen(5000, () => {
+  console.log(`App listening on port: ${PORT}`)
+})
